660b11fb5edd4cb75a141e81e8f3d5ec48047a59
digilau <rickyfukfuk@gmail.com>
2020-07-17 17:14:42 -0700

    fixed the search button for mobile screen

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,7 +6,9 @@ $(document).ready(function () {
 	console.log(listState);
 
 	// set the search button as white when the mouse enter the search button area
-	$('.searchCityInputBtn').mouseenter(function () {
+	$('.searchCityInputBtnJS').mouseenter(function () {
+		console.log('search btn is changing color');
+		event.preventDefault();
 		$('.searchCityInputBtn').remove('.searchCityInputBtnImg1');
 		$('.searchCityInputBtn').html(
 			'<img class="searchCityInputBtnImg2" src="../uwbootscamphomework6/assets/images/searchWhite.png" srcset="../uwbootscamphomework6/assets/images/searchWhite.svg" alt="white search icon"/>'
@@ -14,7 +16,9 @@ $(document).ready(function () {
 	});
 
 	// reset the search button as blue when the mouse exit the search button area
-	$('.searchCityInputBtn').mouseleave(function () {
+	$('.searchCityInputBtnJS').mouseleave(function () {
+		console.log('search btn is returning to orginal color');
+		event.preventDefault();
 		$('.searchCityInputBtn').remove('.searchCityInputBtnImg2');
 		$('.searchCityInputBtn').html(
 			'<img class="searchCityInputBtnImg1" src="../uwbootscamphomework6/assets/images/search.png" srcset="../uwbootscamphomework6/assets/images/search.svg" alt="blue search icon"/>'
@@ -22,20 +26,32 @@ $(document).ready(function () {
 	});
 
 	// unfocus the button when it is not click
-	$('.searchCityInputBtn').mouseup(function blurInput() {
+	$('.searchCityInputBtnJS').mouseup(function blurInput() {
+		// event.preventDefault();
 		$('.searchCityInputBtn').blur();
 	});
 
 	// when the search city button is click
-	$('.searchCityInputBtn').click(function () {
-		event.preventDefault();
+	$('.searchCityInputBtnJS').mousedown(function () {
+		console.log('search btn is clickin');
+		event.stopPropagation();
+		// event.preventDefault();
 		// locate the city name
 		inputByUser = $('#searchCityInput').val();
 		console.log(inputByUser);
 		todayWeather();
 	});
 
-	$('.searchCityInputBtn').click(function () {
+	// // when the search city button is click
+	// $('.searchCityInputBtn').click(function () {
+	// 	event.preventDefault();
+	// 	// locate the city name
+	// 	inputByUser = $('#searchCityInput').val();
+	// 	console.log(inputByUser);
+	// 	todayWeather();
+	// });
+
+	$('.searchCityInputBtnJS').mousedown(function () {
 		event.preventDefault();
 		if (listState !== 'notShow') {
 			$('.searchRecordList').addClass('d-none');
@@ -46,18 +62,22 @@ $(document).ready(function () {
 	// when the search city input box on focus in small screen size
 	// => take out the display none for showing the recent search result
 	$('#searchCityInput').focus(function () {
+		event.preventDefault();
 		if (listState === 'notShow') {
 			$('.searchRecordList').removeClass('d-none');
 			listState = 'show';
 		}
 		// set the search city as bold when the mouse enter the city span
 		$('.searchRecordSpan').mouseenter(function () {
+			event.preventDefault();
 			$(this).addClass('searchRecordHighlight');
 		});
 	});
 
 	// hide the list again when the input box is user click somewhere outside the search city area
-	$('.searchCity').mouseleave(function () {
+	$('#searchCityInput').on('pointerin', function () {
+		console.log('pointout is running');
+		event.preventDefault();
 		if (listState !== 'notShow') {
 			$('.searchRecordList').addClass('d-none');
 			listState = 'notShow';
@@ -78,16 +98,19 @@ $(document).ready(function () {
 
 	// set the search city as bold when the mouse enter the city span
 	$('.searchRecordSpan').mouseenter(function () {
+		event.preventDefault();
 		$(this).addClass('searchRecordHighlight');
 	});
 
 	// reset the search city as normal when the mouse exit the city span
 	$('.searchRecordSpan').mouseleave(function () {
+		event.preventDefault();
 		$(this).removeClass('searchRecordHighlight');
 	});
 
 	// unfocus the city name when it is not click
 	$('.searchRecordSpan').mouseup(function blurInput() {
+		event.preventDefault();
 		$('.searchRecordSpan').blur();
 	});
 });
